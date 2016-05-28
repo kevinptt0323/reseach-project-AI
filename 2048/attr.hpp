e88c2f3cdf84b879fad5aa3e9375a9a4b7dc97aa
bsnipers <jeffjessetwin@yahoo.com.tw>
2016-05-28 16:51:32 +0800

    add attr.hpp

---
+++ 2048/attr.hpp
@@ -0,0 +1,50 @@
+#include "board-kevinptt.hpp"
+
+#define ATTR_NUM 10
+
+int attr_n;
+
+struct Attr
+{
+	int slot_num;
+	int position;
+	int data[1<<24];
+};
+
+Attr attr[ATTR_NUM];
+
+float _score(const board &b)
+{
+	float ret=0;
+	for(int i=0; i<attr_n; i++){
+		int id=0;
+		for(int j=0; j<attr[i].slot_num; j++){
+			int pos=(attr[i].position>>(j<<2))&0xf;
+			id|=b.getCell(pos>>2,pos&0x3)<<(j<<2);
+		}
+		ret+=attr[i].data[id];
+	}
+	return ret;
+}
+
+float score(board b)
+{
+	float ret=0;
+	ret+=_score(b);
+	b.mirrorUD();
+	ret+=_score(b);
+	b.mirrorLR();
+	ret+=_score(b);
+	b.mirrorUD();
+	ret+=_score(b);
+	b.trans();
+	ret+=_score(b);
+	b.mirrorUD();
+	ret+=_score(b);
+	b.mirrorLR();
+	ret+=_score(b);
+	b.mirrorUD();
+	ret+=_score(b);
+	return ret;
+}
+
