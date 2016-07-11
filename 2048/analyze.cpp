1dbbc78f3ef0629b51227a0b6de39b6148863e60
bsnipers <jeffjessetwin@yahoo.com.tw>
2016-07-12 02:52:08 +0800

    output statistic of four divisions

--- 2048/analyze.cpp
+++ 2048/analyze.cpp
@@ -9,6 +9,7 @@
 #include <ctime>
 
 #include <vector>
+#include <map>
 
 using namespace std;
 
@@ -27,9 +28,23 @@ struct data
 	double avg;
 };
 
+
+struct statistic
+{
+	int tupleCnt[20];
+	int gen[5];
+	int learnSpeed[50];
+};
+
 data arr[600];
 
 vector<Attr> zeroAttr[100];
+map<int,int> mmap;
+int tupleNum;
+int position[20];
+
+statistic states[5];
+int division[5]={0,10000,20000,40000,10000000};
 
 char str[100000];
 
@@ -49,16 +64,26 @@ bool load(const char* filename, vector<Attr> &attr) {
 	return true;
 }
 
-
+bool cmp(const data &a, const data &b)
+{
+	return a.avg<b.avg;
+}
 
 int main(int argc, char* argv[]) {
 	char in[2048];
 	vector<Attr> attr;
+	tupleNum=1;
 	for(int ID=1; ID<=60; ID++){
 		sprintf(in,"zero%d.dat",ID);
 		if( !load(in, zeroAttr[ID]) ) {
 			fprintf(stderr, "file open failed.\n");
 			return 1;
+		}
+		for(int j=0; j<(int)zeroAttr[ID].size(); j++){
+			if(mmap.count(zeroAttr[ID][j].position)==0){
+				position[tupleNum]=zeroAttr[ID][j].position;
+				mmap[zeroAttr[ID][j].position]=tupleNum++;
+			}
 		}
 	}
 	FILE* res=fopen("result.csv","r");
@@ -82,6 +107,51 @@ int main(int argc, char* argv[]) {
 	}
 	for(int i=0; i<540; i++){
 		arr[i].avg/=1000;
+	}
+	sort(arr,arr+540,cmp);
+	for(int i=0; i<540; i++){
+		int tar=-1,ID=arr[i].set.ID;
+		for(int j=0; j<5; j++)
+			if(arr[i].avg>division[j])
+				tar=j;
+		for(int j=0; j<(int)zeroAttr[ID].size(); j++)
+			states[tar].tupleCnt[mmap[zeroAttr[ID][j].position]]++;
+		states[tar].gen[arr[i].set.gen]++;
+		states[tar].learnSpeed[arr[i].set.speed]++;
+	}
+	for(int i=1; i<tupleNum; i++){
+		int sum=0;
+		for(int j=0; j<5; j++){
+			sum+=states[j].tupleCnt[i];
+		}
+		printf("%3d ",sum);
+	}
+	puts("");
+	for(int i=0; i<5; i++){
+		for(int j=1; j<tupleNum; j++)
+			printf("%3d ",states[i].tupleCnt[j]);
+		puts("");
+		for(int j=1; j<=3; j++)
+			printf("%d ",states[i].gen[j]);
+		puts("");
+		printf("%d %d %d\n",states[i].learnSpeed[5],states[i].learnSpeed[10],states[i].learnSpeed[40]);
+		puts("");
+	}
+	for(int i=1; i<tupleNum; i++){
+		int pos[10];
+		int slotNum=0;
+		for(int j=0; j<3 || (position[i]>>(j<<2)); j++)
+			slotNum++;
+		for(int j=0; j<slotNum; j++){
+			pos[j]=(position[i]>>(j<<2))&0xf;
+		}
+		board b;
+		memset(b.row,0,sizeof(b.row));
+		for(int j=0; j<slotNum; j++){
+			b.setCell(pos[j]>>2,pos[j]&0x3,j+1);
+		}
+		printf("tuple ID: %d\n",i);
+		b.print();
 	}
 	return 0;
 }
