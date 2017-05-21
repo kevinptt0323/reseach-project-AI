4b79ce4ef1d4f5cfbd3bd3f878d51dd90cadeb2d
Kevin Lin <kevinptt0323@gmail.com>
2017-05-21 21:16:37 +0800

    use argv[2] to assign playing turns

--- 2048/main.cpp
+++ 2048/main.cpp
@@ -71,15 +71,16 @@ int main(int argc, char* argv[]) {
 	genMap();
 	board b;
 	{
+#ifdef _WIN32
+		srand(time(0));
+#else
 		random_device rd;
 		minstd_rand RNG(rd());
 		uniform_int_distribution<int> uid(0, 32768);
 		srand(uid(RNG));
+#endif
 	}
-	b.init();
-	b.print();
-	int sum=0;
-	if( argc==2 ) {
+	if( argc>=2 ) {
 		char in[2048];
 		strcpy(in, argv[1]);
 		vector<Attr> attr;
@@ -92,40 +93,57 @@ int main(int argc, char* argv[]) {
 		moveArr[1]=&board::down;
 		moveArr[2]=&board::left;
 		moveArr[3]=&board::right;
-		int score=0;
-		do{
-			board newb[4];
-			int earnScore[4], tar=-1;
-			float tarScore, tmpScore;
-			for(int i=0; i<4; i++){
-				newb[i]=b;
-				earnScore[i]=(newb[i].*moveArr[i])(false);
-				if(earnScore[i]!=-INF){
-					tmpScore = earnScore[i]+getScore(newb[i],attr);
-					if(tar==-1 || tmpScore>tarScore){
-						tar=i;
-						tarScore = tmpScore;
+		int round = 1;
+		if (argc==3) round = atoi(argv[2]);
+		long long sum = 0;
+		int maxScore = 0;
+		for(int _=1; _<=round; _++) {
+			b.init();
+			if (round==1) b.print();
+			int score = 0;
+			do{
+				board newb[4];
+				int earnScore[4], tar=-1;
+				float tarScore, tmpScore;
+				for(int i=0; i<4; i++){
+					newb[i]=b;
+					earnScore[i]=(newb[i].*moveArr[i])(false);
+					if(earnScore[i]!=-INF){
+						tmpScore = earnScore[i]+getScore(newb[i],attr);
+						if(tar==-1 || tmpScore>tarScore){
+							tar=i;
+							tarScore = tmpScore;
+						}
 					}
 				}
+				if(tar==-1)
+					break;
+				score+=(b.*moveArr[tar])(false);
+				b.genCell();
+			} while(1);
+			sum += score;
+			maxScore = max(maxScore, score);
+			if (round==1) {
+				b.print();
+				printf("%d\n",score);
 			}
-			if(tar==-1)
-				break;
-			score+=(b.*moveArr[tar])(false);
-			b.genCell();
-		}while(1);
-		b.print();
-		printf("%d\n",score);
+		}
+		printf("Max Score:\t%d\n", maxScore);
+		printf("Average Score:\t%.2f\n", 1.*sum/round);
 		for(int i=0; i<(int)attr.size(); i++) {
 			delete[] attr[i].data;
 		}
 	} else {
+		int score=0;
+		b.init();
+		b.print();
 		char str[10];
 		while( ~scanf("%s", str) ) {
 			switch(str[0]) {
-				case 'a': printf("total score: %d\n",sum+=b.left(true)); break;
-				case 'd': printf("total score: %d\n",sum+=b.right(true)); break;
-				case 'w': printf("total score: %d\n",sum+=b.up(true)); break;
-				case 's': printf("total score: %d\n",sum+=b.down(true)); break;
+				case 'a': printf("total score: %d\n",score+=b.left(true)); break;
+				case 'd': printf("total score: %d\n",score+=b.right(true)); break;
+				case 'w': printf("total score: %d\n",score+=b.up(true)); break;
+				case 's': printf("total score: %d\n",score+=b.down(true)); break;
 			}
 			b.print();
 			puts("");
